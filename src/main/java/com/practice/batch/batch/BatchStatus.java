402aa72fcf58e3582ae0bedc5953519d2e6b0a9a
kyoungbeom <pkb1998@naver.com>
2024-12-12 00:57:19 +0900

    Spring Batch를 사용하지 않고 배치 프로그램 구현하기

---
+++ src/main/java/com/practice/batch/batch/BatchStatus.java
@@ -0,0 +1,15 @@
+package com.practice.batch.batch;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public enum BatchStatus {
+
+    STARING,
+    FAILED,
+    COMPLETED
+    ;
+
+}
---
+++ src/test/java/com/practice/batch/DormantBatchJobTest.java
@@ -0,0 +1,129 @@
+package com.practice.batch;
+
+import com.practice.batch.batch.BatchStatus;
+import com.practice.batch.batch.JobExecution;
+import com.practice.batch.customer.Customer;
+import com.practice.batch.customer.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class DormantBatchJobTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private DormantBatchJob dormantBatchJob;
+
+    @BeforeEach
+    public void setUp() {
+        customerRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("로그인 시간이 일년을 경과한 고객이 세명이고, 일년 이내에 로그인한 고객이 다섯명이면 세명의 고객이 휴면전환대상이다.")
+    void test1() {
+
+        // given
+        saveCustomer(366);
+        saveCustomer(366);
+        saveCustomer(366);
+        saveCustomer(364);
+        saveCustomer(364);
+        saveCustomer(364);
+        saveCustomer(364);
+        saveCustomer(364);
+
+        // when
+        final JobExecution result = dormantBatchJob.execute();
+
+        // then
+        final long dormantCount = customerRepository.findAll()
+                .stream()
+                .filter(it -> it.getStatus() == Customer.Status.DORMANT)
+                .count();
+
+        assertThat(dormantCount).isEqualTo(3);
+        assertThat(result.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+
+    }
+
+    @Test
+    @DisplayName("고객이 열명이 있지만 모두 다 휴먼전환대상이면(로그인 한지 1년 경과한 사람) 휴면전황대상은 10명이다.")
+    void test2() {
+
+        // given
+        saveCustomer(366);
+        saveCustomer(366);
+        saveCustomer(366);
+        saveCustomer(366);
+        saveCustomer(366);
+        saveCustomer(366);
+        saveCustomer(366);
+        saveCustomer(366);
+        saveCustomer(366);
+        saveCustomer(366);
+
+        // when
+        final JobExecution result = dormantBatchJob.execute();
+
+        // then
+        final long dormantCount = customerRepository.findAll()
+                .stream()
+                .filter(it -> it.getStatus() == Customer.Status.DORMANT)
+                .count();
+
+        assertThat(dormantCount).isEqualTo(10);
+        assertThat(result.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+
+    }
+
+    @Test
+    @DisplayName("고객이 없는 경우에도 배치는 정상동작해야한다.")
+    void Test() {
+
+        // when
+        final JobExecution result = dormantBatchJob.execute();
+
+        // then
+        final long dormantCount = customerRepository.findAll()
+                .stream()
+                .filter(it -> it.getStatus() == Customer.Status.DORMANT)
+                .count();
+
+        assertThat(dormantCount).isEqualTo(0);
+        assertThat(result.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+
+    }
+
+    @Test
+    @DisplayName("배치가 실패하면 BatchStatus는 FAILED를 반환해야 한다")
+    void test4() {
+        // given
+        final DormantBatchJob dormantBatchJob = new DormantBatchJob(null);
+
+        // when
+        final JobExecution result = dormantBatchJob.execute();
+
+        // then
+        assertThat(result.getStatus()).isEqualTo(BatchStatus.FAILED);
+    }
+
+    private void saveCustomer(long loginMinusDays) {
+        final String uuid = UUID.randomUUID().toString();
+        final Customer testCustomer = new Customer(uuid, uuid + "@practice.com");
+
+        testCustomer.setLoginAt(LocalDateTime.now().minusDays(loginMinusDays));
+        customerRepository.save(testCustomer);
+    }
+
+}
